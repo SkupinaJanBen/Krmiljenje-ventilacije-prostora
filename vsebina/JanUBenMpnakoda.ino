c31be9a6231f53d775858e8d5dae0bcb667c71e3
SkupinaJanBen <skupina.jbm@gmail.com>
2024-06-13 09:18:32 +0200

    Update JanUBenMpnakoda.ino

--- vsebina/JanUBenMpnakoda.ino
+++ vsebina/JanUBenMpnakoda.ino
@@ -16,22 +16,22 @@ void setup()
 
 void loop() 
 {
-  int EmanuelValue = analogRead(A1);
-  EmanuelValue = map(EmanuelValue, 0, 1023, 249, 1023);
-  int EmanuelValue1 = EmanuelValue / 4;
+  int RP1 = analogRead(A1);
+  RP1 = map(EmanuelValue, 0, 1023, 249, 1023);
+  int EmanuelValue = RP1 / 4;
   int AvtoValue = analogRead(A0);
   AvtoValue = map(AvtoValue, 0, 1023, 144, 1023);
-  int AvtoValue1 = AvtoValue / 4;
-  Serial.println(AvtoValue1);
+  int AvtoValue = AvtoValue / 4;
+  Serial.println(AvtoValue);
   
   
   if (digitalRead(4)== HIGH && digitalRead(3)== HIGH) {
-    analogWrite(5, AvtoValue1);
+    analogWrite(5, AvtoValue);
     digitalWrite(LED2, HIGH);
     digitalWrite(LED1, LOW);
   }
   else if (digitalRead(4) == HIGH && digitalRead(3)== LOW) {
-    analogWrite(5, EmanuelValue1);
+    analogWrite(5, EmanuelValue);
     digitalWrite(LED2, LOW);
     digitalWrite(LED1, HIGH);
   }
